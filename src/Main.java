c535cc1d27d2f702c344740d59cf8dbfe53b24c7
BT_1E10_14 <example@bths.edu>
2022-03-02 12:11:00 -0500

    duplicate

--- src/Main.java
+++ src/Main.java
@@ -10,95 +10,32 @@ public class Main {
                 {"Brain", "Elenor", "Harry", "Lenny"},
                 {"Cathy", "Fred", "Jill", "Matt"}
         };
+        int [][] no=  {{0,1,2},{3,4,5},{8,9,6}};
+        int [][] onr=  {{1,2,3},{9,2,0},{8,9,6}};
+        int [][] two=  {{1,3,6},{3,4,6},{1,5,8}};
+        int [][] one2=  {{1,2,3},{1,4,6}};
 
-        printStringMatrix(seatingChart);
+        for (int[] row: no){
+            System.out.println( Arrays.toString(row));
+        }
+        System.out.println("Expected result is: false with 0 duplicates" );
+        System.out.println("Actual result is "+ columnDuplicates(no));
+        for (int[] row: onr){
+            System.out.println( Arrays.toString(row));
+        }
+        System.out.println("Expected result is: true with 1 duplicates" );
+        System.out.println("Actual result is "+ columnDuplicates(onr));
+        for (int[] row: two){
+            System.out.println( Arrays.toString(row));
+        }
+        System.out.println("Expected result is: true with 2 duplicates" );
+        System.out.println("Actual result is "+ columnDuplicates(two));
+        for (int[] row: one2){
+            System.out.println( Arrays.toString(row));
+        }
+        System.out.println("Expected result is: true with 1 duplicates" );
+        System.out.println("Actual result is "+ columnDuplicates(one2));
 
-        //1b
-        System.out.println("\n1b");
-        seatingChart[1][2] = "Paul";
-
-        printStringMatrix(seatingChart);
-
-        //1c
-        System.out.println("\n1c");
-        String tempStudent = seatingChart[0][0];
-        seatingChart[0][0] = seatingChart[2][3];
-        seatingChart[2][3] = tempStudent;
-
-        printStringMatrix(seatingChart);
-
-        //1d
-        System.out.println("\n1d");
-        String[] tempRow = seatingChart[0];
-        seatingChart[0] = seatingChart[1];
-        seatingChart[1] = tempRow;
-
-        printStringMatrix(seatingChart);
-
-        //2a
-        System.out.println("\n2a");
-        int[][] arr1 = new int[2][3];
-        int[][] arr2 = new int[3][2];
-
-        printIntMatrix(arr1);
-        System.out.println("");
-        printIntMatrix(arr2);
-
-        //2b
-        System.out.println("\n2b");
-
-        arr1[0][0] = 1;
-        arr1[0][1] = 2;
-        arr1[0][2] = 3;
-        arr1[1][0] = 4;
-        arr1[1][1] = 5;
-        arr1[1][2] = 6;
-
-        printIntMatrix(arr1);
-
-        //2c
-        System.out.println("\n2c");
-        arr2[0][0] = 1;
-        arr2[1][0] = 2;
-        arr2[2][0] = 3;
-        arr2[0][1] = 4;
-        arr2[1][1] = 5;
-        arr2[2][1] = 6;
-
-        printIntMatrix(arr2);
-
-        //2D Array Methods
-        System.out.println("\nTest code for totalElements");
-        int[][] testArr1 = {{5, 2}, {1, 3}, {7, 9}};
-        System.out.println(FunWith2DArrays.totalElements(testArr1));
-        int[][] testArr2 = {{1, 2, 3}, {6, 5, 4}};
-        System.out.println(FunWith2DArrays.totalElements(testArr2));
-        int[][] testArr3 = {{1, 2, 3, 4}, {7, 8, 9, 10}};
-        System.out.println(FunWith2DArrays.totalElements(testArr3));
-        int[][] testArr4 = {{4, 5, 2}, {1, 9, 7}, {8, 10, 13}};
-        System.out.println(FunWith2DArrays.totalElements(testArr4));
-        int[][] testArr5 = {{4, 5, 2, 4, 1}, {1, 9, 7, 8, 2}, {8, 10, 13, 4, 3}};
-        System.out.println(FunWith2DArrays.totalElements(testArr5));
-        int[][] testArr6 = {{4, 5, 2, 4, 8, 9, 10}, {1, 9, 7, 8, 6, 1, 2}};
-        System.out.println(FunWith2DArrays.totalElements(testArr6));
-        int[][] testArr7 = {{4}, {7}, {8}, {2}};
-        System.out.println(FunWith2DArrays.totalElements(testArr7));
-        int[][] testArr8 = {{4}};
-        System.out.println(FunWith2DArrays.totalElements(testArr8));
-        int[][] testArr9 = {{}};
-        System.out.println(FunWith2DArrays.totalElements(testArr9));
-
-        System.out.println("\nTest code for fourCorners");
-        String[][] words = {{"hi", "bye", "stuff", "go"}, {"time", "up", "you", "good"}, {"map", "low", "bow", "mom"}};
-        FunWith2DArrays.fourCorners(words);
-        String[][] words2 = {{"time", "up", "bye"}, {"hi", "hit", "up"}, {"map", "bam", "low"}, {"bow", "mom", "joy"}};
-        FunWith2DArrays.fourCorners(words2);
-        String[][] words3 = {{"phone", "mouse", "keyboard"}};
-        FunWith2DArrays.fourCorners(words3);
-        String[][] words4 = {{"cat"}, {"dog"}, {"hamster"}, {"bird"}};
-        FunWith2DArrays.fourCorners(words4);
-        String[][] words5 = {{"time"}};
-        FunWith2DArrays.fourCorners(words5);
 
 
     }
@@ -106,7 +43,7 @@ public class Main {
     {
         for(String[] row : stringMatrix)
         {
-            System.out.println(Arrays.toString(row));
+            //System.out.println(Arrays.toString(row));
         }
     }
 
@@ -114,8 +51,26 @@ public class Main {
     {
         for(int[] row : intMatrix)
         {
-            System.out.println(Arrays.toString(row));
+            //System.out.println(Arrays.toString(row));
         }
+    }
+
+    public static String  columnDuplicates(int [][] arr){
+        int num=0;
+        boolean k =false;
+        for (int f=0; f<arr[0].length; f++){
+            for (int s=0; s< arr.length; s++){
+                int ge=arr[s][f];
+                for (int d=s+1;d< arr.length; d++) {
+                    if (ge == arr[d][f]) {
+                        num++;
+                        k= true;
+                    }
+                }
+            }
+
+        }
+        return  k+ ": Because, there are "+ num+ " duplicates in the 2d array";
     }
 
 }
